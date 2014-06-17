6ade88e97cf9a5819df8143d2802a246967f5776
zwjlpeng@gmail.com <zwjlpeng@gmail.com>
2014-06-17 10:06:29 +0800

    commit

--- sds.c
+++ sds.c
@@ -28,6 +28,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+//发生内存错误时终止程序
 #define SDS_ABORT_ON_OOM
 
 #include "sds.h"
@@ -38,58 +39,90 @@
 #include <ctype.h>
 #include "zmalloc.h"
 
+//内存出错函数
 static void sdsOomAbort(void) {
+    //发生内存错误时向标准输出设备输出出错信息
     fprintf(stderr,"SDS: Out Of Memory (SDS_ABORT_ON_OOM defined)\n");
-    abort();
+    abort();//出错时直接终止程序
 }
 
+//新建一个字符串，并将其初始化，初始化时，取init字符串中initlen个字符
 sds sdsnewlen(const void *init, size_t initlen) {
     struct sdshdr *sh;
-
+    //创建字符串存储空间，多创建一个1，是因为C语言中的字符串
+    //均有一个\0结束符
     sh = zmalloc(sizeof(struct sdshdr)+initlen+1);
 #ifdef SDS_ABORT_ON_OOM
+    //如果内存分配失败的情况下，直接终止程序的运行
     if (sh == NULL) sdsOomAbort();
 #else
+    //如果内存分配失败的情况下，返回NULL
     if (sh == NULL) return NULL;
 #endif
+    //记录内存空间的实际使用量
     sh->len = initlen;
+    //记录内存空间的余下多少
     sh->free = 0;
     if (initlen) {
+        //如果设置了init的情况下，复制initlen个字符
         if (init) memcpy(sh->buf, init, initlen);
+        //否则将内存全部置为0
         else memset(sh->buf,0,initlen);
     }
+    //结尾字符,C语言中字符串均以\0结尾
     sh->buf[initlen] = '\0';
     return (char*)sh->buf;
 }
 
+//生成一个空字符串
 sds sdsempty(void) {
     return sdsnewlen("",0);
 }
 
+/*
+ * 生成一个新字符串
+ * init表字生成新字符串时要拷贝进来的东西
+ */
 sds sdsnew(const char *init) {
     size_t initlen = (init == NULL) ? 0 : strlen(init);
     return sdsnewlen(init, initlen);
 }
 
+/*
+ * 获取字符串的长度
+ * 字符串的长度是存放在sdshdr结构中的len里面
+ */
 size_t sdslen(const sds s) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     return sh->len;
 }
 
+/*
+ * 对字符串进行复制
+ */
 sds sdsdup(const sds s) {
     return sdsnewlen(s, sdslen(s));
 }
 
+/*
+ * 释放字符串
+ */
 void sdsfree(sds s) {
     if (s == NULL) return;
     zfree(s-sizeof(struct sdshdr));
 }
 
+/*
+ * 返回字符串中还有多少空间可以使用
+ */
 size_t sdsavail(sds s) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     return sh->free;
 }
 
+/*
+ * 更新字符串的长度
+ */
 void sdsupdatelen(sds s) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     int reallen = strlen(s);
@@ -97,26 +130,31 @@ void sdsupdatelen(sds s) {
     sh->len = reallen;
 }
 
+/*
+ * 给字符串增加长度
+ */
 static sds sdsMakeRoomFor(sds s, size_t addlen) {
     struct sdshdr *sh, *newsh;
     size_t free = sdsavail(s);
     size_t len, newlen;
-
+    //如果余下空间还足够的话
     if (free >= addlen) return s;
     len = sdslen(s);
     sh = (void*) (s-(sizeof(struct sdshdr)));
     newlen = (len+addlen)*2;
     newsh = zrealloc(sh, sizeof(struct sdshdr)+newlen+1);
+    //内存分配失败的情况下进行处理
 #ifdef SDS_ABORT_ON_OOM
     if (newsh == NULL) sdsOomAbort();
 #else
     if (newsh == NULL) return NULL;
 #endif
-
+    //新的余下空间
     newsh->free = newlen - len;
     return newsh->buf;
 }
 
+//进行字符串的拼接操作
 sds sdscatlen(sds s, void *t, size_t len) {
     struct sdshdr *sh;
     size_t curlen = sdslen(s);
@@ -131,12 +169,15 @@ sds sdscatlen(sds s, void *t, size_t len) {
     return s;
 }
 
+//同样进行字符串的拼接操作
 sds sdscat(sds s, char *t) {
     return sdscatlen(s, t, strlen(t));
 }
 
+//进行字符串的copy,会覆盖掉原字符串
 sds sdscpylen(sds s, char *t, size_t len) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
+    //字符串s代表的字符串的总共长度
     size_t totlen = sh->free+sh->len;
 
     if (totlen < len) {
@@ -152,10 +193,13 @@ sds sdscpylen(sds s, char *t, size_t len) {
     return s;
 }
 
+//同样也是进行字符串的copy
+//也会覆盖掉原字符串
 sds sdscpy(sds s, char *t) {
     return sdscpylen(s, t, strlen(t));
 }
 
+//格式化拼接字符串
 sds sdscatprintf(sds s, const char *fmt, ...) {
     va_list ap;
     char *buf, *t;
@@ -169,6 +213,7 @@ sds sdscatprintf(sds s, const char *fmt, ...) {
         if (buf == NULL) return NULL;
 #endif
         buf[buflen-2] = '\0';
+        //c语言中的可变参数的循环遍历
         va_start(ap, fmt);
         vsnprintf(buf, buflen, fmt, ap);
         va_end(ap);
@@ -179,28 +224,36 @@ sds sdscatprintf(sds s, const char *fmt, ...) {
         }
         break;
     }
+    //对字符串进行拼接
     t = sdscat(s, buf);
     zfree(buf);
     return t;
 }
 
+//字符串中去掉首尾包含cset里的字符
 sds sdstrim(sds s, const char *cset) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     char *start, *end, *sp, *ep;
     size_t len;
-
+    //字符串的开始位置
     sp = start = s;
+    //字符串的结束位置
     ep = end = s+sdslen(s)-1;
+    //查看字符串cset中首次出现字符*sp的位置
     while(sp <= end && strchr(cset, *sp)) sp++;
+    //查找字符串cset中首次出现字符*ep的位置
     while(ep > start && strchr(cset, *ep)) ep--;
     len = (sp > ep) ? 0 : ((ep-sp)+1);
+    //字符串的内存移动操作
     if (sh->buf != sp) memmove(sh->buf, sp, len);
     sh->buf[len] = '\0';
+    //字符串的余下空间增长
     sh->free = sh->free+(sh->len-len);
     sh->len = len;
     return s;
 }
 
+//获取指定范围里的字符串
 sds sdsrange(sds s, long start, long end) {
     struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     size_t newlen, len = sdslen(s);
@@ -229,22 +282,22 @@ sds sdsrange(sds s, long start, long end) {
     return s;
 }
 
+//将字符串中的所有字符均转为小写的形式
 void sdstolower(sds s) {
     int len = sdslen(s), j;
-
     for (j = 0; j < len; j++) s[j] = tolower(s[j]);
 }
 
+//将字符串中所有字符均转为大写的形式
 void sdstoupper(sds s) {
     int len = sdslen(s), j;
-
     for (j = 0; j < len; j++) s[j] = toupper(s[j]);
 }
 
+//对两个字符串进行比较
 int sdscmp(sds s1, sds s2) {
     size_t l1, l2, minlen;
     int cmp;
-
     l1 = sdslen(s1);
     l2 = sdslen(s2);
     minlen = (l1 < l2) ? l1 : l2;
@@ -271,7 +324,8 @@ int sdscmp(sds s1, sds s2) {
  */
 sds *sdssplitlen(char *s, int len, char *sep, int seplen, int *count) {
     int elements = 0, slots = 5, start = 0, j;
-
+    //开设5字节的内存空间
+    //实际开设时会开设6字节，具体可以参看zmallo.c里的源代码
     sds *tokens = zmalloc(sizeof(sds)*slots);
 #ifdef SDS_ABORT_ON_OOM
     if (tokens == NULL) sdsOomAbort();
@@ -295,12 +349,13 @@ sds *sdssplitlen(char *s, int len, char *sep, int seplen, int *count) {
         }
         /* search the separator */
         if ((seplen == 1 && *(s+j) == sep[0]) || (memcmp(s+j,sep,seplen) == 0)) {
+            //计算分割符以前的字符串的长度
             tokens[elements] = sdsnewlen(s+start,j-start);
             if (tokens[elements] == NULL) {
 #ifdef SDS_ABORT_ON_OOM
-                sdsOomAbort();
+                sdsOomAbort();//失败的情况下，如果定义了内存失败函数，将直接调用
 #else
-                goto cleanup;
+                goto cleanup;//否则跳到cleanup函数，进行一个一个的释放
 #endif
             }
             elements++;
@@ -309,6 +364,7 @@ sds *sdssplitlen(char *s, int len, char *sep, int seplen, int *count) {
         }
     }
     /* Add the final element. We are sure there is room in the tokens array. */
+    //分割串里面的最后一个字符串
     tokens[elements] = sdsnewlen(s+start,len-start);
     if (tokens[elements] == NULL) {
 #ifdef SDS_ABORT_ON_OOM
@@ -318,10 +374,13 @@ sds *sdssplitlen(char *s, int len, char *sep, int seplen, int *count) {
 #endif
     }
     elements++;
+    //传的是个引用，可以获取最终分割为多少个数组
     *count = elements;
     return tokens;
 
 #ifndef SDS_ABORT_ON_OOM
+//如果没有定义分配失败的情况下，内存操作函数
+//将会一个一个的调用内存失败处理函数
 cleanup:
     {
         int i;

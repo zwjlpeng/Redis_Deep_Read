8e9925788d7db2293f1ad43c930edb33058054ee
zwjlpeng@gmail.com <zwjlpeng@gmail.com>
2014-06-26 11:07:58 +0800

    commit

--- anet.c
+++ anet.c
@@ -45,37 +45,63 @@
 
 #include "anet.h"
 
+/**
+ * 设置与网络相关的错误
+ */
 static void anetSetError(char *err, const char *fmt, ...)
 {
     va_list ap;
-
+    //表示错误信息要存放的位置
+    //如果没有为错误信息的存放设置缓存的话
     if (!err) return;
     va_start(ap, fmt);
+    //#define ANET_ERR_LEN 256
     vsnprintf(err, ANET_ERR_LEN, fmt, ap);
     va_end(ap);
 }
 
+/**
+ * 将文件描述符fd设置为非阻塞的方式
+ * 其实就是将socket设置为非阻塞方式
+ */
 int anetNonBlock(char *err, int fd)
 {
     int flags;
-
+    /**
+     * fcntl函数可以改变已打开的文件性质
+     */
     /* Set the socket nonblocking.
      * Note that fcntl(2) for F_GETFL and F_SETFL can't be
      * interrupted by a signal. */
+    /**
+     * 获取文件描述符fd上的标志位
+     */
     if ((flags = fcntl(fd, F_GETFL)) == -1) {
+        //如果获取失败的情况下，将错误信息输出到标准错误输出设备上
         anetSetError(err, "fcntl(F_GETFL): %s\n", strerror(errno));
         return ANET_ERR;
     }
+    //给当前的文件描述符设置非阻塞标志位
     if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        //将错误信息输出到标准出错设备上
         anetSetError(err, "fcntl(F_SETFL,O_NONBLOCK): %s\n", strerror(errno));
         return ANET_ERR;
     }
     return ANET_OK;
 }
 
+/**
+ * 将Tcp报文设置为非延迟方式 
+ */
+/**
+ * int setsockopt(SOCKET s, int level, int optname,const char *optval,int optlen)
+ * s(套接字)指向一个打开的套接字描述字
+ * 
+ */
 int anetTcpNoDelay(char *err, int fd)
 {
     int yes = 1;
+    //将TCP报文设置为非延迟方式
     if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes)) == -1)
     {
         anetSetError(err, "setsockopt TCP_NODELAY: %s\n", strerror(errno));
@@ -84,8 +110,12 @@ int anetTcpNoDelay(char *err, int fd)
     return ANET_OK;
 }
 
+/**
+ * 设置TCP发送时的报文结构
+ */
 int anetSetSendBuffer(char *err, int fd, int buffsize)
-{
+{   
+    //设置TCP发送时的缓存空间的大小
     if (setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &buffsize, sizeof(buffsize)) == -1)
     {
         anetSetError(err, "setsockopt SO_SNDBUF: %s\n", strerror(errno));
@@ -93,7 +123,9 @@ int anetSetSendBuffer(char *err, int fd, int buffsize)
     }
     return ANET_OK;
 }
-
+/**
+ * 设置TCP保持活动链接
+ */
 int anetTcpKeepAlive(char *err, int fd)
 {
     int yes = 1;
@@ -104,6 +136,7 @@ int anetTcpKeepAlive(char *err, int fd)
     return ANET_OK;
 }
 
+//对主机进行解析
 int anetResolve(char *err, char *host, char *ipbuf)
 {
     struct sockaddr_in sa;
@@ -125,11 +158,17 @@ int anetResolve(char *err, char *host, char *ipbuf)
 
 #define ANET_CONNECT_NONE 0
 #define ANET_CONNECT_NONBLOCK 1
+/**
+ * 以TCP的方式来连接主机
+ */
 static int anetTcpGenericConnect(char *err, char *addr, int port, int flags)
 {
     int s, on = 1;
     struct sockaddr_in sa;
 
+    /**
+     * 创建socket套接字
+     */
     if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         anetSetError(err, "creating socket: %s\n", strerror(errno));
         return ANET_ERR;
@@ -140,6 +179,10 @@ static int anetTcpGenericConnect(char *err, char *addr, int port, int flags)
 
     sa.sin_family = AF_INET;
     sa.sin_port = htons(port);
+    /**
+     * inet_aton是一个改进的方法，用来将一个字符串IP地址转换为一个
+     * 32位的网络序列地址
+     */
     if (inet_aton(addr, &sa.sin_addr) == 0) {
         struct hostent *he;
 
@@ -167,11 +210,17 @@ static int anetTcpGenericConnect(char *err, char *addr, int port, int flags)
     return s;
 }
 
+/**
+ * 以阻塞的方式连接主机
+ */
 int anetTcpConnect(char *err, char *addr, int port)
 {
     return anetTcpGenericConnect(err,addr,port,ANET_CONNECT_NONE);
 }
 
+/**
+ * 以非阻塞的方式连接主机
+ */
 int anetTcpNonBlockConnect(char *err, char *addr, int port)
 {
     return anetTcpGenericConnect(err,addr,port,ANET_CONNECT_NONBLOCK);
@@ -179,6 +228,9 @@ int anetTcpNonBlockConnect(char *err, char *addr, int port)
 
 /* Like read(2) but make sure 'count' is read before to return
  * (unless error or EOF condition is encountered) */
+/**
+ * 从socket里面读取count个字节放到buf中
+ */
 int anetRead(int fd, char *buf, int count)
 {
     int nread, totlen = 0;
@@ -194,9 +246,14 @@ int anetRead(int fd, char *buf, int count)
 
 /* Like write(2) but make sure 'count' is read before to return
  * (unless error is encountered) */
+/**
+ * 向socket里面写入count个字节
+ */
 int anetWrite(int fd, char *buf, int count)
 {
     int nwritten, totlen = 0;
+    //加上此循环的好处是，如果没有写完的话
+    //继续向里面写
     while(totlen != count) {
         nwritten = write(fd,buf,count-totlen);
         if (nwritten == 0) return totlen;
@@ -207,6 +264,10 @@ int anetWrite(int fd, char *buf, int count)
     return totlen;
 }
 
+/**
+ * 将TCP Socket绑定到指定的端口上
+ * 并开始进行监听
+ */
 int anetTcpServer(char *err, int port, char *bindaddr)
 {
     int s, on = 1;
@@ -245,6 +306,9 @@ int anetTcpServer(char *err, int port, char *bindaddr)
     return s;
 }
 
+/**
+ * 从套接字上开始接收数据
+ */
 int anetAccept(char *err, int serversock, char *ip, int *port)
 {
     int fd;
